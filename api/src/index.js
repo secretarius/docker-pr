b419e36639b602f64aca41849934a4d744dc0680
Alex <olexandrmai@gmail.com>
2020-07-14 01:34:05 +0300

    cr docker pr

---
+++ api/src/index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const mongoose = require("mongoose")
+const { connectDb } = require("./helpers/db");
+const { host, port, db } = require("./configuration")
+const app = express();
+
+const postSchema = new mongoose.Schema({
+  name: String
+});
+
+const Post = mongoose.model("Post", postSchema);
+
+
+
+const startServer = () => {
+  app.listen(port, () => {
+    console.log(`Started api service on port: ${port}`);
+    console.log(`On host: ${host}`);
+    console.log(`Our database: ${db}`);
+
+    // Post.find(function(err, posts) {
+    //   if(err) return console.error(err);
+    //   console.log("posts", posts);
+    // })
+
+    const silence = new Post({name: "Silence"});
+    silence.save(function(err, savedSilence) {
+      if (err) return console.error(err);
+      console.log("savedSilence with volume", savedSilence);
+    });
+  });
+};
+
+app.get("/test", (req, res) => {
+  res.send("Our api server is working correctly");
+});
+
+connectDb()
+  .on('error', console.log)
+  .on('disconnected', connectDb)
+  .once('open', startServer);
